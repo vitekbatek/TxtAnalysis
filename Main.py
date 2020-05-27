2098d6c6c18900201af5b20b7b9b9707f0a957ad
vitekbatek <vitekbatek@gmail.com>
2020-05-27 17:13:42 +0700

    Fix 2

--- Main.py
+++ Main.py
@@ -4,39 +4,52 @@ from Lib.CheckFile import check
 from Lib.ReadFile import read
 from Lib.GetFilenames import getnames
 
+#Get data
 now = datetime.datetime.now()
-#Input path
-try:
-    path = str(input('Введите Директорию для проверки: '))
-except ValueError:
-    print("Директория для проверки введена неверно\n")
-    exit(0)
-if path == "":
-    print("Директория для проверки не введена\n")
-    exit(0)
-if not os.path.exists(path):
-    print('Директория не найдена')
-    exit(0)
+
+
 
 #Write logfile
 with open(str(os.getcwd())+"/checklogs.txt", "w") as filelogs:
+    filelogs.write(
+        'Проверка текстовых файлов НПРЧ на дублирование значений по столбцам\nДата выполнения: ' + now.strftime(
+            "%d-%m-%Y %H:%M") + '\n')
 
-    filenames = getnames(path)
-    filelogs.write("Проверка файлов. Дата выполнения " + now.strftime("%d-%m-%Y %H:%M") + ':\n')
+    # Input path
+    try:
+        path = str(input('Введите Директорию для проверки: '))
+    except ValueError:
+        filelogs.write('Директория для проверки введена неверно. Проверка будет выполнена в текущей директории\n')
+        print('Директория для проверки введена неверно. Проверка будет выполнена в текущей директории')
+        path = os.getcwd()
+    if path == '':
+        filelogs.write('Директория для проверки не введена. Проверка будет выполнена в текущей директории\n')
+        print('Директория для проверки не введена. Проверка будет выполнена в текущей директории')
+        path = os.getcwd()
+    if not os.path.exists(path):
+        filelogs.write('Директория не найдена. Проверка будет выполнена в текущей директории\n')
+        print('Директория не найдена. Проверка будет выполнена в текущей директории')
+        path = os.getcwd()
+
+    filelogs.write('Директория: ' + str(path) + '\n')
 
     try:
         columns = list(map(int, input('Введите через пробел номера столбцов по которым необходимо сделать проверку:  ').split(" ")))
     except ValueError:
-        filelogs.write("Осуществлен некорректный ввод столбцов\n")
-        print("Осуществлен некорректный ввод столбцов\n")
+        filelogs.write('Осуществлен некорректный ввод столбцов. Проверка не выполнена\n')
+        print('Осуществлен некорректный ввод столбцов. Проверка не выполнена')
         exit(0)
 
 
+    filenames = getnames(path)
 
     if columns == []:
-        filelogs.write("Столбцы для проерки не введены\n")
-        print("Столбцы для проерки не введены\n")
+        filelogs.write('   Столбцы для проверки не введены\n')
+        print('Столбцы для проверки не введены')
         exit(0)
+    else:
+        filelogs.write('Столбцы для проверки:' + str(columns) + '\n')
+        print('Столбцы для проверки:' + str(columns))
     print('Проверка:', end=' ')
 
     i = 6
@@ -47,13 +60,10 @@ with open(str(os.getcwd())+"/checklogs.txt", "w") as filelogs:
             print('')
         i+=1
         # Read Lines to List
-        #print(filename)
         listlines = read(str(filename))
         filelogs.write(filename + '\n')
 
         # Check List
-        #columns = [2, 3]
         listerr = check(listlines, columns)
-        #print("   " + str(listerr))
         for err in reversed(listerr):
             filelogs.write(err + '\n')
